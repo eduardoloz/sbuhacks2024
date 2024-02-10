b84497039409ba641cc60dffa680d61477d87ab9
joh9911 <joh9911@naver.com>
2024-02-11 05:21:28 +0900

    conflict solved

--- App.js
+++ App.js
@@ -3,42 +3,20 @@ import { StyleSheet, Text, View, Button} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import { NavigationContainer } from '@react-navigation/native';
 
-<<<<<<< HEAD
+
 import dummy from "./screens/dummy";
 import MainPage from "./screens/MainPage";
-=======
-// import dummyScreen from './screens/'
-import FeedScreen from './screens/FeedScreen';
-import CreateScreen from './screens/CreateScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import SearchScreen from './screens/SearchScreen';
->>>>>>> c4d23e6 (tried to add buttons)
+
 
 const Stack = createNativeStackNavigator();
 
-// // code for the nextPage button
-// const handleSubmit = () => {
 
 
-// };
 
 
-//export default function App() {
 const App = () => {
 
-  // const [username, setUsername] = useState('');
-
-  // // code for the Welcome screen
-  // return (
-  //   <View style={styles.container}>
-  //     <Text>Welcome! Please press the button below:</Text>
-  //     {/* <Button title="Submit" onPress={handleSubmit} /> */}
-  //     <Button title="Next" 
-  //             onPress={() => navigation.navigate('ProfileScreen')} />
-  //   </View>
-  // );
 
   return (
     <NavigationContainer>
@@ -50,20 +28,5 @@ const App = () => {
   );
 }
 
-<<<<<<< HEAD
-=======
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-
-});
-
-
-
 export default App;
->>>>>>> c4d23e6 (tried to add buttons)
+
