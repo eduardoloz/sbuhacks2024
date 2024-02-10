81c2b5b7889f1a9f9702c5d6fde05ae7b6244ad0
eduardoloz <eddieslozano@gmail.com>
2024-02-10 13:18:57 -0500

    repeat line removed

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 //import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
 
 import FeedScreen from './screens/FeedScreen';
 import CreateScreen from './screens/CreateScreen';
